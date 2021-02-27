cf547eb19c261f75cf6cf2c8cfb6aa3334683bd7
Noah Portes Chaikin <noah@porteschaikin.com>
2021-02-27 15:59:39 -0500

    Progress on recorder

--- recorder/src/app.js
+++ recorder/src/app.js
@@ -1,66 +1,58 @@
 const sdk = require('@onflow/sdk');
 const types = require('@onflow/types');
+const interaction = require('@onflow/interaction');
 
 const TOPSHOT_NODE = 'https://access-mainnet-beta.onflow.org';
 const TOPSHOT_ADDRESS = '0b2a3299cc857e29';
-const TOPSHOT_MARKET_ADDRESS = '0b2a3299cc857e29';
+const TOPSHOT_MARKET_ADDRESS = 'c1e4f4f4c4257510';
 const TOPSHOT_EVENT_TYPE = 'A.c1e4f4f4c4257510.Market.MomentPurchased';
-
-const TOPSHOT_SALE_MOMENT_SCRIPT = `
-  import TopShot from 0x0b2a3299cc857e29
-  import Market from 0xc1e4f4f4c4257510
-
-  pub struct SaleMoment {
-    pub var id: UInt64
-    pub var playId: UInt32
-    pub var play: {String: String}
-    pub var setId: UInt32
-    pub var setName: String
-    pub var serialNumber: UInt32
-    pub var price: UFix64
-
-    init(moment: &TopShot.NFT, price: UFix64) {
-      self.id = moment.id
-      self.playId = moment.data.playID
-      self.play = TopShot.getPlayMetaData(playID: self.playId)!
-      self.setId = moment.data.setID
-      self.setName = TopShot.getSetName(setID: self.setId)!
-      self.serialNumber = moment.data.serialNumber
-      self.price = price
-    }
-  }
-
-  pub fun main(owner:Address, momentID:UInt64): SaleMoment {
-    let acct = getAccount(owner)
-    let collectionRef =
-      acct.getCapability(/public/topshotSaleCollection)!.borrow<&{Market.SalePublic}>() ?? panic("Could not borrow capability from public collection")
-
-    return SaleMoment(
-      moment: collectionRef.borrowMoment(id: momentID)!,
-      price:  collectionRef.getPrice(tokenID: momentID)!
-    )
-  }
-`;
 
 const opts = {node: TOPSHOT_NODE};
 
-const saleMomentForEvent = async function(event) {
+const saleMomentForEvent = async function(event, height) {
   const payload = event.payload;
   const value = payload.value;
   const moment = value.fields[0];
   const seller = value.fields[2];
 
-  console.log(moment);
+  const script = `
+    import TopShot from 0x${TOPSHOT_ADDRESS}
+    import Market from 0x${TOPSHOT_MARKET_ADDRESS}
+
+    pub struct SaleMoment {
+      pub var id: UInt64
+      pub var playId: UInt32
+      pub var play: {String: String}
+      pub var setId: UInt32
+      pub var setName: String
+      pub var serialNumber: UInt32
+      pub var price: UFix64
+
+      init(moment: &TopShot.NFT, price: UFix64) {
+        self.id = moment.id
+        self.playId = moment.data.playID
+        self.play = TopShot.getPlayMetaData(playID: self.playId)!
+        self.setId = moment.data.setID
+        self.setName = TopShot.getSetName(setID: self.setId)!
+        self.serialNumber = moment.data.serialNumber
+        self.price = price
+      }
+    }
+
+    pub fun main(): SaleMoment {
+      let acct = getAccount(${seller.value.value.value})
+      let collection =
+        acct.getCapability(/public/topshotSaleCollection)!.borrow<&{Market.SalePublic}>() ?? panic("Could not borrow capability from public collection")
+      return SaleMoment(
+        moment: collection.borrowMoment(id: ${moment.value.value})!,
+        price:  collection.getPrice(tokenID: ${moment.value.value})!
+      )
+    }
+  `;
+
   const response = await sdk.send(
     await sdk.pipe(
-      await sdk.build([
-        sdk.args([
-          sdk.arg(seller.value.value.value, types.Address),
-          sdk.arg(1349317, types.UInt64),
-        ]),
-        sdk.script(TOPSHOT_SALE_MOMENT_SCRIPT),
-      ]),
-      [sdk.resolve([sdk.resolveArguments, sdk.resolveParams])],
+      await sdk.build([sdk.getBlockByHeight(height), sdk.script(script)]),
     ),
     opts,
   );
@@ -78,7 +70,7 @@ const run = async function() {
     await sdk.build([
       sdk.getEvents(
         TOPSHOT_EVENT_TYPE,
-        blockResponse.block.height - 2000,
+        blockResponse.block.height - 5000,
         blockResponse.block.height,
       ),
     ]),
@@ -87,7 +79,7 @@ const run = async function() {
 
   const events = [eventsResponse.events[0]];
   events.forEach(function(event) {
-    const moment = saleMomentForEvent(event);
+    const moment = saleMomentForEvent(event, blockResponse.block.height);
   });
 };
 
