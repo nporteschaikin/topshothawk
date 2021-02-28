753eee3d8bd7641b0fb7a9e7c9353c564439f8fc
Noah Portes Chaikin <noah@porteschaikin.com>
2021-02-28 16:41:56 -0500

    More progress on recorder script

--- recorder/src/app.js
+++ recorder/src/app.js
@@ -7,79 +7,99 @@ const TOPSHOT_ADDRESS = '0b2a3299cc857e29';
 const TOPSHOT_MARKET_ADDRESS = 'c1e4f4f4c4257510';
 const TOPSHOT_EVENT_TYPE = 'A.c1e4f4f4c4257510.Market.MomentPurchased';
 
-const opts = {node: TOPSHOT_NODE};
+const TOPSHOT_SALE_MOMENT_SCRIPT = `
+import TopShot from 0x${TOPSHOT_ADDRESS}
+import Market from 0x${TOPSHOT_MARKET_ADDRESS}
 
-const saleMomentForEvent = async function(event, height) {
+pub struct SaleMoment {
+  pub var id: UInt64
+  pub var playId: UInt32
+  pub var play: {String: String}
+  pub var setId: UInt32
+  pub var setName: String
+  pub var serialNumber: UInt32
+  pub var price: UFix64
+
+  init(moment: &TopShot.NFT, price: UFix64) {
+    self.id = moment.id
+    self.playId = moment.data.playID
+    self.play = TopShot.getPlayMetaData(playID: self.playId)!
+    self.setId = moment.data.setID
+    self.setName = TopShot.getSetName(setID: self.setId)!
+    self.serialNumber = moment.data.serialNumber
+    self.price = price
+  }
+}
+
+pub fun main(seller: Address, momentID: UInt64): SaleMoment {
+  let acct = getAccount(seller)
+  let collection =
+    acct.getCapability(/public/topshotSaleCollection)!.borrow<&{Market.SalePublic}>() ?? panic("Could not borrow capability from public collection")
+
+  return SaleMoment(
+    moment: collection.borrowMoment(id: momentID)!,
+    price:  collection.getPrice(tokenID: momentID)!
+  )
+}
+`;
+
+const send = async function(req) {
+  return await sdk.send(req, {node: TOPSHOT_NODE});
+};
+
+const fetchMomentAtParentBlock = async function(event, block) {
   const payload = event.payload;
   const value = payload.value;
   const moment = value.fields[0];
   const seller = value.fields[2];
 
-  const script = `
-    import TopShot from 0x${TOPSHOT_ADDRESS}
-    import Market from 0x${TOPSHOT_MARKET_ADDRESS}
+  const interaction = await sdk.build([
+    sdk.script(TOPSHOT_SALE_MOMENT_SCRIPT),
+    sdk.atBlockId(block.parentId),
+    sdk.args([
+      sdk.arg(seller.value.value.value, types.Address),
+      sdk.arg(parseInt(moment.value.value), types.UInt64),
+    ]),
+  ]);
 
-    pub struct SaleMoment {
-      pub var id: UInt64
-      pub var playId: UInt32
-      pub var play: {String: String}
-      pub var setId: UInt32
-      pub var setName: String
-      pub var serialNumber: UInt32
-      pub var price: UFix64
+  const pipe = await sdk.pipe(interaction, [
+    sdk.resolveArguments,
+    sdk.resolveParams,
+  ]);
 
-      init(moment: &TopShot.NFT, price: UFix64) {
-        self.id = moment.id
-        self.playId = moment.data.playID
-        self.play = TopShot.getPlayMetaData(playID: self.playId)!
-        self.setId = moment.data.setID
-        self.setName = TopShot.getSetName(setID: self.setId)!
-        self.serialNumber = moment.data.serialNumber
-        self.price = price
-      }
-    }
-
-    pub fun main(): SaleMoment {
-      let acct = getAccount(${seller.value.value.value})
-      let collection =
-        acct.getCapability(/public/topshotSaleCollection)!.borrow<&{Market.SalePublic}>() ?? panic("Could not borrow capability from public collection")
-      return SaleMoment(
-        moment: collection.borrowMoment(id: ${moment.value.value})!,
-        price:  collection.getPrice(tokenID: ${moment.value.value})!
-      )
-    }
-  `;
-
-  const response = await sdk.send(
-    await sdk.pipe(
-      await sdk.build([sdk.getBlockByHeight(height), sdk.script(script)]),
-    ),
-    opts,
-  );
+  const response = await send(pipe);
 
   console.log(response);
 };
 
+const fetchLatestBlock = async function() {
+  const interaction = await sdk.build([sdk.getLatestBlock()]);
+
+  const response = await send(interaction);
+  return response.block;
+};
+
+const fetchEventsAtBlock = async function(block) {
+  const interaction = await sdk.build([
+    sdk.getEvents(TOPSHOT_EVENT_TYPE, block.height - 5000, block.height),
+  ]);
+
+  const response = await send(interaction);
+  return response.events;
+};
+
+const processEvent = async function(event, block) {
+  const moment = await fetchMomentAtParentBlock(event, block);
+  console.log(moment);
+};
+
 const run = async function() {
-  const blockResponse = await sdk.send(
-    await sdk.build([sdk.getLatestBlock()]),
-    opts,
-  );
+  const block = await fetchLatestBlock();
+  const events = await fetchEventsAtBlock(block);
 
-  const eventsResponse = await sdk.send(
-    await sdk.build([
-      sdk.getEvents(
-        TOPSHOT_EVENT_TYPE,
-        blockResponse.block.height - 5000,
-        blockResponse.block.height,
-      ),
-    ]),
-    opts,
-  );
-
-  const events = [eventsResponse.events[0]];
-  events.forEach(function(event) {
-    const moment = saleMomentForEvent(event, blockResponse.block.height);
+  // await events.forEach(function(event) {
+  await [events[0]].forEach(function(event) {
+    processEvent(event, block);
   });
 };
 
