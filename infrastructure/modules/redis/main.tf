4a987d4b33bb3f73d57654c43dd006e0c11a2131
Noah Portes Chaikin <noah@porteschaikin.com>
2021-03-04 09:29:56 -0500

    Bump Redis version

--- infrastructure/modules/redis/main.tf
+++ infrastructure/modules/redis/main.tf
@@ -22,8 +22,8 @@ resource "aws_elasticache_replication_group" "replication_group" {
   engine                        = "redis"
   node_type                     = "cache.t2.medium"
   number_cache_clusters         = 1
-  parameter_group_name          = "default.redis3.2"
-  engine_version                = "3.2.10"
+  parameter_group_name          = "default.redis4.0"
+  engine_version                = "4.0.10"
   port                          = 6379
   subnet_group_name             = aws_elasticache_subnet_group.subnet_group.id
   security_group_ids            = [aws_security_group.security_group.id]
